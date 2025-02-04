6b54256e9c3750013e1ad8359843db82fe96548a
Ronit Sohal <ronitsohal@gmail.com>
2025-02-04 14:03:46 -0500

    Fixed text size in about me page (About me Page is now for real done)

--- src/AboutMe/AboutCard.jsx
+++ src/AboutMe/AboutCard.jsx
@@ -2,7 +2,7 @@ export function AboutCard({ title, content }) {
     return (
       <div className="space-y-2">
         <h2 className="text-sm text-gray-400 font-[customFont]">{title}</h2>
-        <p className="text-gray-300 leading-relaxed text-base font-[customFont]">{content}</p>
+        <p className="text-gray-300 leading-relaxed text-lg font-[customFont]">{content}</p>
       </div>
     )
 }
