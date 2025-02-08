0b745ce9b17cf77ac7a0dc9397b2eecf1b863d8b
Ronit Sohal <ronitsohal@gmail.com>
2025-02-08 00:09:47 -0500

    Implemented project carousel for the project cards, projects page should be done

--- src/AboutMe/AboutMePageData.jsx
+++ src/AboutMe/AboutMePageData.jsx
@@ -28,25 +28,25 @@ export const spotifyData = [
     label: "music",
     bgColor: "bg-blue-600/20",
     url: 'https://open.spotify.com/embed/track/2WAwqgBGJb4igeTF07n6OG?utm_source=generator',
-    imageUrl: "../../public/pictures/IMG_5585.jpg",
+    imageUrl: "../../public/pictures/AboutMePics/IMG_5585.jpg",
   },
   {
     label: "drives",
     bgColor: "bg-red-600/20",
     url: 'https://open.spotify.com/embed/track/1Es7AUAhQvapIcoh3qMKDL?utm_source=generator',
-    imageUrl: "../../public/pictures/IMG_1943.jpg",
+    imageUrl: "../../public/pictures/AboutMePics/IMG_1943.jpg",
   },
   {
     label: "creative",
     bgColor: "bg-[#282828]/90",
     url: 'https://open.spotify.com/embed/track/3xKsf9qdS1CyvXSMEid6g8?utm_source=generator',
-    imageUrl: "../../public/pictures/IMG_7422.jpg",
+    imageUrl: "../../public/pictures/AboutMePics/IMG_7422.jpg",
   },
   {
     label: "flow",
     bgColor: "bg-teal-600/20",
     url: 'https://open.spotify.com/embed/track/5NAOhPAZ57eFAXr3qatS6i?utm_source=generator',
-    imageUrl: "../../public/pictures/IMG_0192.jpg",
+    imageUrl: "../../public/pictures/AboutMePics/IMG_0192.jpg",
   },
 ]
 
---
+++ src/Projects/ProjectsData.jsx
@@ -0,0 +1,38 @@
+export const projects = [
+    {
+        title: "Personal Website",
+        description: "My personal website made using React, Tailwind, and Three.js.",
+        imageUrl: "/pictures/ProjectPics/portfolio-project.jpg",
+        githubUrl: "https://github.com/ronitsohal05/portfolio",
+    },
+    {
+        title: "Connect App",
+        description: "An app to help elderly combat social isolation, made using Java, XML, and Google Firebase.",
+        imageUrl: "/pictures/ProjectPics/connect.jpg",
+        githubUrl: "https://github.com/ronitsohal05/Connect",
+    },
+    {
+        title: "Snapscript",
+        description: "Script to increase snapscore, made using Selenium",
+        imageUrl: "/pictures/ProjectPics/snapscript-project.jpg",
+        githubUrl: "https://github.com/ronitsohal05/SnapScript",
+    },
+    {
+        title: "AMC-AIME-Bot",
+        description: "Discord bot scrapes problems from past AMC and AIME tests, made using Discord.py and Selenium",
+        imageUrl: "/pictures/ProjectPics/AMC-AIME-BOT.jpg",
+        githubUrl: "https://github.com/ronitsohal05/AMC-AIME-BOT",
+    },
+    {
+        title: "Tic Tak Toe",
+        description: "Made Tik-Tak-Toe game AI implementing the minimax algorithm, made using python and pygame",
+        imageUrl: "/pictures/ProjectPics/tictaktoe.jpg",
+        githubUrl: "https://github.com/ronitsohal05/Tic-Tak-Toe-Game",
+    },
+    {
+        title: "Corporate Stability Predictor",
+        description: "Implemented various ML models such as AdaBoost, Decision Tree, Random Forest Classifier, and K-Neighbors Classifier ",
+        imageUrl: "/pictures/ProjectPics/csm.jpg",
+        githubUrl: "https://github.com/ronitsohal05/Corporate-Stability-Predictor/tree/main",
+    }
+];
